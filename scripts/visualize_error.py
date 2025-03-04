281aa1b5a098e1d5aea40b473851a9a6fc0072a9
Carlos Pereira <carlosp28@gmail.com>
2025-03-04 17:11:51 +0000

    Visualize error in long forecast results

--- scripts/visualize_error.py
+++ scripts/visualize_error.py
@@ -3,32 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Input the paths associated with the forecasts/reference datasets in weatherbench format
-paradis_path = "forecast_result.zarr"
-base_path = "weatherbench_5.625deg_13level/"
+paradis_path = "results/forecast_result.zarr"
+base_path = "/home/cap003/hall6/weatherbench_raw/weatherbench_5.625deg_13level/"
+# base_path = "/home/cap003/hall6/weatherbench_raw/weatherbench_1.5deg_13level/"
 
 # Generate plots with forecast data
 ds_paradis = xr.open_dataset(paradis_path, engine="zarr")
 ds_base = xr.open_mfdataset(base_path, engine="zarr")
 
-date = "2020-01-01T00:00:00"
-end_date = "2020-01-02"
+# Period of forecast input (forecasts start 6h after)
+date = "2020-01-01"
+end_date = "2020-01-31"
+
 forecast_ind = 0
 
-ds_base = ds_base.sel(time=slice(date, end_date)).isel(time=forecast_ind + 1)
-ds_paradis = ds_paradis.sel(time=date).isel(prediction_timedelta=forecast_ind)
+ds_paradis = ds_paradis.sel(time=slice(date, end_date)).isel(
+    prediction_timedelta=forecast_ind
+)
+
+# Transform PARADIS's time dimension to be that of the forecast at that time
+ds_paradis["time"] = ds_paradis.time + ds_paradis.prediction_timedelta
+
+# Extract the appropriate time instances from the base
+ds_base = ds_base.sel(time=ds_paradis.time)
+
+# Clean up xarrays to match
+ds_paradis = ds_paradis.drop_vars("prediction_timedelta")
+ds_paradis = ds_paradis.reindex_like(ds_base)
+
+# Align arrays
+ds_paradis, ds_base = xr.align(ds_paradis, ds_base, join="inner")
 
 # Get the latitude and longitude grid
 longitude, latitude = np.meshgrid(ds_base.longitude.values, ds_base.latitude.values)
 
-g_base = ds_base.sel(level=500)["geopotential"].values.transpose()
-g_para = ds_paradis.sel(level=500)["geopotential"].values
+g_base = ds_base.sel(level=500).geopotential.compute()
+g_para = ds_paradis.sel(level=500).geopotential.compute()
 
 # Get a simple pointwise error
-error_paradis = g_para - g_base
+diff = g_para - g_base
 
-# Reduce value using RMSE adn MAE
-rmse_paradis = np.sqrt(np.mean(((error_paradis) / 10) ** 2))
-mae_paradis = np.mean(np.abs((error_paradis) / 10))
+rmse_paradis = ((diff / 10) ** 2).mean(dim=["time", "latitude", "longitude"]) ** 0.5
+mae_paradis = (np.abs(diff / 10)).mean(dim=["time", "latitude", "longitude"])
 
 fig, ax = plt.subplots(ncols=3, figsize=(18, 5))
 figs = [plt.figure(figsize=(8, 4)) for i in range(3)]
@@ -38,13 +54,13 @@ vmax = np.max(g_base)
 vmin = np.min(g_base)
 
 # Plot base results
-ax[0].contourf(longitude, latitude, g_base, 100, cmap="RdYlBu_r", vmax=vmax, vmin=vmin)
+ax[0].contourf(longitude, latitude, g_base[0].T, 100, cmap="RdYlBu_r", vmax=vmax, vmin=vmin)
 
 # Plot PARADIS results
-ax[1].contourf(longitude, latitude, g_para, 100, cmap="RdYlBu_r", vmax=vmax, vmin=vmin)
+ax[1].contourf(longitude, latitude, g_para[0], 100, cmap="RdYlBu_r", vmax=vmax, vmin=vmin)
 
 # Generate a pointwise error contour
-contours = ax[2].imshow(error_paradis, cmap="hot_r")
+contours = ax[2].imshow(diff[0], cmap="hot_r")
 plt.colorbar(contours)
 
 ax[0].set_title("GZ500 ERA5")
@@ -55,7 +71,6 @@ plt.tight_layout()
 
 for i, fig in enumerate(figs):
     fig.savefig(f"gz500_{i}.png")
-
 # Show result to screen
 print("GZ500-RMSE-PARADIS", f"{rmse_paradis:.2f}m")
 print("GZ500-MAE-PARADIS", f"{mae_paradis:.2f}m")
